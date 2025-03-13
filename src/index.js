5b4fa5c0201ab7ad1e5cf0f0e6fc21080f729797
Ryan Bates <ryan@railscasts.com>
2025-03-13 13:28:17 -0700

    Add renderHtmlFromBrowser function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import { parse } from './parser.js';
 import { renderHtml } from './renderers/html.js';
 
-export { parse, renderHtml };
+function renderHtmlFromBrowser(markup, renderOptions = {}) {
+  const commands = parse(markup);
+  const createCanvas = (width, height) => {
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    return canvas;
+  };
+  return renderHtml({ commands, createCanvas, ...renderOptions });
+}
+
+export { parse, renderHtml, renderHtmlFromBrowser };
